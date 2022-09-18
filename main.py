fd0d883d2fdd979ff69bedef98112e543b7ba219
jk185256 <jk185256@ncr.com>
2022-09-18 15:25:36 -0500

    bug_fix_summation_of_data_plot_lines

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ access_key = "place key here" # DO NOT show in commits; todo: get from file
 request_ticker = ""
 response = ""
 api_results = [] # declare list of responses for each api call
-tickers = ['TSLA', 'NCR', 'ETH', 'AMZN', 'XOM'] # todo get from input control
+#tickers = ['TSLA', 'NCR', 'ETH', 'AMZN', 'XOM'] # todo get from input control
+tickers = ['AMZN', 'ETH', 'NCR', 'TSLA', 'XOM'] # todo get from input control
 api_get = "http://api.marketstack.com/v1/eod" # todo: get from file
 for ticker in tickers: # loop through ticker list
     request_ticker = ticker
     params = {
     "access_key" : access_key,
     "symbols" : request_ticker}
-    response = requests.get(api_get, params) #get call; todo: wrap in try except
+    response = requests.get(api_get, params) #get call; todo: wrap in try except; log except
     api_results.append(response) 
 for result in api_results:
     if result.status_code == 200: #status code check
@@ -33,13 +34,17 @@ for result in api_results:
             labels.append(ftime)            
             data_closeprice.append(item['close'])
         closeprice_nparray = np.array(data_closeprice) # convert data list to numpy array
+        tickername = item['symbol']
+        plt.figure() # must create figure object to prevent summation
         plt.plot(closeprice_nparray) # move all plt.xxxx to a method
         plt.gca().invert_xaxis() # reverse x axis
         plt.ylabel('Close price')
-        plt.title(item['symbol'])
+        plt.title(tickername)
         plt.xticks(np.arange(10), labels, rotation = 30)        
         plt.margins(.05, .1) # padding
-        plt.show()
-    else:
+        plt.tight_layout()
+        plt.savefig(f"charts/{tickername}.png") # todo: wrap in try except, log except
+        # plt.show() # uncomment to show
+    else: 
         print(f"\n\tAPI Call failed with status code {result.status_code}.") # error message if not status code 200
 
